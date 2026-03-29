10ce81b714fabcbbaff4d9c48e884a0d57e060d1
Rebahi M'hamed Wassim <wassimrbh00@gmail.com>
2026-03-29 20:17:09 +0200

    Fix PORT assignment to use logical OR

--- backend/src/server.js
+++ backend/src/server.js
@@ -55,7 +55,7 @@ const start = async () => {
 
     socketHandler(io);
 
-    const PORT = process.env.PORT  5000;
+    const PORT = process.env.PORT || 5000;
 
     server.listen(PORT, '0.0.0.0', () => {
       console.log('Server running on port ' + PORT);
